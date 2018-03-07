58789bd56cd26808f93f8931eb1383a2a50eb467
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-03-07 14:48:42 -0500

    Scaffold basic header component

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,5 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Logo, Menu } from '../components/Icons';
 
-const Header = () => <div />;
+const StyledHeader = styled.header`
+  align-content: space-between;
+  align-items: end;
+  background-color: ${props => props.backgroundColor};
+  display: grid;
+  grid-template: auto / auto 1fr auto;
+  left: 0;
+  padding: 16px;
+  position: fixed;
+  right: 0;
+  top: 0;
+`;
+
+const LogoLink = styled.a`
+  display: block;
+  height: 24px;
+  grid-column: 1 /2;
+`;
+
+const MenuLink = styled.div`
+  align-self: end;
+  color: #fff;
+  cursor: pointer;
+  font-size: 1.2rem;
+  grid-column: 3 / 4;
+  height: 24px;
+  justify-self: end;
+`;
+
+const Header = () => (
+  <StyledHeader backgroundColor="#0094ff">
+    <LogoLink href="https://taxfoundation.org">
+      <Logo fill="#fff" />
+    </LogoLink>
+    <MenuLink>
+      <Menu fill="#fff" />
+    </MenuLink>
+  </StyledHeader>
+);
 
 export default Header;
