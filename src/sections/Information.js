b0ca207a86fc5b936a1b56e434dc8ea323c68f8c
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-03-08 13:34:23 -0500

    Fill out basic info section

--- src/sections/Information.js
+++ src/sections/Information.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import SectionContainer from '../components/SectionContainer';
+import { fullDate } from '../utilities/formatters';
 
-const Informations = () => <div />;
+const InfoHeading = styled.h2`
+  font-size: 1.8rem !important;
+  line-height: 1.4 !important;
+`;
+
+const Informations = props => {
+  let date = new Date(props.date);
+
+  return (
+    <SectionContainer>
+      <InfoHeading>{`${fullDate(date)} at ${props.venue}`}</InfoHeading>
+      <p>{props.address}</p>
+    </SectionContainer>
+  );
+};
 
 export default Informations;
