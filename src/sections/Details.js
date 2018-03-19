d47af15fde09cb5f5d6fed5f10b383e9495a55cf
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-03-19 09:37:13 -0400

    Add image grid

--- src/sections/Details.js
+++ src/sections/Details.js
@@ -2,6 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import BackgroundColorContainer from '../components/BackgroundColorContainer';
 import SectionContainer from '../components/SectionContainer';
+import PhotoA from '../images/a.jpg';
+import PhotoB from '../images/b.jpg';
+import PhotoC from '../images/c.jpg';
+import PhotoD from '../images/d.jpg';
+import PhotoE from '../images/e.jpg';
+import PhotoF from '../images/f.jpg';
+import PhotoG from '../images/g.jpg';
+import PhotoH from '../images/h.jpg';
 
 const TaglineContainer = SectionContainer.extend`
   padding: 2rem 3rem;
@@ -9,12 +17,6 @@ const TaglineContainer = SectionContainer.extend`
   @media (min-width: 500px) {
     padding: 5rem 9rem;
   }
-`;
-
-const DetailsContainer = styled.div`
-  display: grid;
-  grid-gap: 1rem;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 `;
 
 const DetailsList = styled.ul`
@@ -40,6 +42,23 @@ const StatisticsList = styled.ul`
   }
 `;
 
+const PhotoGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(4, 25vh);
+
+  @media (min-width: 1000px) {
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, 25vh);
+  }
+`;
+
+const Photo = styled.div`
+  background-image: url(${props => props.src});
+  background-size: cover;
+  background-position: 50% 50%;
+`;
+
 const Details = props => (
   <div id={props.id}>
     <BackgroundColorContainer bg="primary" color="white">
@@ -48,13 +67,22 @@ const Details = props => (
         <p>{props.details.detailsDescription}</p>
       </TaglineContainer>
     </BackgroundColorContainer>
+    <BackgroundColorContainer bg="primary" color="white">
+      <PhotoGrid>
+        <Photo src={PhotoA} />
+        <Photo src={PhotoB} />
+        <Photo src={PhotoC} />
+        <Photo src={PhotoD} />
+        <Photo src={PhotoE} />
+        <Photo src={PhotoF} />
+        <Photo src={PhotoG} />
+        <Photo src={PhotoH} />
+      </PhotoGrid>
+    </BackgroundColorContainer>
     <BackgroundColorContainer bg="secondary" color="white">
       <SectionContainer>
         <h3>Why You Can't Miss Tax Prom</h3>
-        <DetailsContainer>
-          <DetailsList>{props.details.detailsBullets.map((d, i) => <li key={`detail-${i}`}>{d}</li>)}</DetailsList>
-          <div>Photos Go Here</div>
-        </DetailsContainer>
+        <DetailsList>{props.details.detailsBullets.map((d, i) => <li key={`detail-${i}`}>{d}</li>)}</DetailsList>
         <h4>In 2017 Tax Prom was Joined By</h4>
         <StatisticsList statsNumber={props.details.detailsStatistics.length}>
           {props.details.detailsStatistics.map((d, i) => <li key={`stat-${i}`}>{d}</li>)}
