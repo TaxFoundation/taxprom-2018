1437f18423030e412dfe694e17364a6df28096d9
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-08-29 12:38:09 -0400

    Add basic info on winner

--- src/sections/Awards.js
+++ src/sections/Awards.js
@@ -2,15 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 import BackgroundColorContainer from '../components/BackgroundColorContainer';
 import SectionContainer from '../components/SectionContainer';
+import mw from '../images/mark-weinberger.jpg';
+
+const AwardeeContainer = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template: auto / repeat(2, 1fr);
+`;
+
+const Name = styled.div`
+  border-bottom: 1px solid #fff;
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+`;
 
 const Awards = () => (
   <BackgroundColorContainer bg="primary" color="white">
     <SectionContainer>
       <h3>2018 Distiguished Service Award</h3>
-      <div>
-        <p>Winner: Guy</p>
-        <p>Have a picture here.</p>
-      </div>
+      <AwardeeContainer>
+        <div>
+          <Name>
+            <h4>Mark Weinberger</h4>
+            <p style={{ textAlign: 'center' }}>Global Chairman and CEO of EY</p>
+          </Name>
+          <p>Bio goes here.</p>
+        </div>
+        <div>
+          <img src={mw} alt="Mark Weinberger" />
+        </div>
+      </AwardeeContainer>
     </SectionContainer>
   </BackgroundColorContainer>
 );
